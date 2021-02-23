5106190b44dfd7d6dc35339bfb0ecbf142973b73
Michal Toman <michalt@balena.io>
2021-02-23 09:42:22 +0100

    Add endpoints for creating and fetching SSL certificates
    Change-type: minor
    Signed-off-by: Michal Toman <michalt@balena.io>

---
+++ balenasign/app/balenasign/cert.py
@@ -0,0 +1,57 @@
+import logging
+import os
+import subprocess
+
+from balenasign.utils import X509_DIR
+
+
+LOG = logging.getLogger("cert")
+
+
+def get(cert_id):
+    cert_filename = "%s.crt" % cert_id
+    cert_path = os.path.join(X509_DIR, cert_filename)
+    if not os.path.isfile(cert_path):
+        return {"error": "Certificate '%s' not found" % cert_id}, 404
+
+    with open(cert_path, "r") as f:
+        cert_data = f.read(1 << 26)
+
+    response = {"cert": cert_data}
+    return response
+
+
+def new(body, user):
+    cert_id = body["cert_id"]
+
+    cert_filename = "%s.crt" % cert_id
+    cert_path = os.path.join(X509_DIR, cert_filename)
+    if os.path.isfile(cert_path):
+        return {"error": "Certificate '%s' already exists" % cert_id}, 409
+
+    key_filename = "%s.key" % cert_id
+    key_path = os.path.join(X509_DIR, key_filename)
+    if os.path.isfile(key_path):
+        # The error message is intentionally the same
+        return {"error": "Certificate '%s' already exists" % cert_id}, 409
+
+    key_length = body.get("key_length", 2048)
+
+    cmd = [
+        "openssl", "req", "-new", "-x509", "-newkey", "rsa:%d" % key_length,
+        "-subj", body["subject"], "-keyout", key_path, "-out", cert_path,
+        "-days", "3650", "-sha512", "-nodes"
+    ]
+
+    retcode = subprocess.call(cmd)
+    if retcode != 0:
+        return {"error": "Failed to generate certificate"}, 500
+
+    with open(cert_path, "r") as f:
+        cert_data = f.read(1 << 26)
+
+    response = {"cert": cert_data}
+
+    LOG.info("%s successfully generated a new certificate %s", user, cert_id)
+
+    return response
