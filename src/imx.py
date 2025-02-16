f4022e59974d1a1473240c24386245ce822668e8
Alex Gonzalez <alexg@balena.io>
2025-02-16 17:42:56 +0100

    imx: Fix linter warnings
    Change-type: patch
    Signed-off-by: Alex Gonzalez <alexg@balena.io>

--- src/imx.py
+++ src/imx.py
@@ -31,13 +31,14 @@ def pkis():
     }
     return response
 
+
 def generate_hab(hab_type, tree_dir):
     """
     Generates a PKI tree based of the provided type (hab4/ahab)
     """
     srks = "-num-srk 4"
     dig_algo = None
-    script="hab4_pki_tree.sh"
+    script = "hab4_pki_tree.sh"
     if hab_type == "ahab":
         script = "ahab_pki_tree.sh"
         srks = None
@@ -55,11 +56,11 @@ def generate_hab(hab_type, tree_dir):
         file.write(datetime.now().strftime('%Y%m%d'))
     # Generate a random password file
     with open(f"{PKI_DIR}/keys/key_pass.txt", 'w', encoding='utf-8') as file:
-        password=base64.b64encode(os.urandom(32)).decode('utf-8')
+        password = base64.b64encode(os.urandom(32)).decode('utf-8')
         file.write(f"{password}\n{password}")
 
     # Call key generation script
-    cmd = [ f"{tree_dir}/{script}",
+    cmd = [f"{tree_dir}/{script}",
            "-existing-ca", "n",
            "-kt", "rsa",
            "-kl", "4096",
@@ -80,8 +81,8 @@ def generate_hab(hab_type, tree_dir):
         raise RuntimeError("Failed to generate PKI tree")
 
     # Organize
-    shutil.move(f"{PKI_DIR}/crts", f"{tree_dir}/" )
-    shutil.move(f"{PKI_DIR}/keys", f"{tree_dir}/" )
+    shutil.move(f"{PKI_DIR}/crts", f"{tree_dir}/")
+    shutil.move(f"{PKI_DIR}/keys", f"{tree_dir}/")
     # Cleanup
     os.remove(f"{tree_dir}/{script}")
     for ext in ['sh', 'old', 'attr', 'txt']:
@@ -94,7 +95,7 @@ def generate_srks(hab_type, tree_dir):
     Generates the SRK table and efuse hash
     """
     srk_table = "SRK_table.bin"
-    hab_ver="--hab_ver 4"
+    hab_ver = "--hab_ver 4"
     srk_digest = "digest sha256"
     if hab_type == "ahab":
         hab_ver = "--ahab_ver"
@@ -102,10 +103,10 @@ def generate_srks(hab_type, tree_dir):
     # Beware this sort will only work for up to 9 certicates as SRK10 will
     # be sortered after SRK1.
     csv_certs = ','.join(sorted(glob.glob(f"{tree_dir}/crts/SRK*crt.pem")))
-    cmd = [ "srktool",
-            "--table", f"{tree_dir}/{srk_table}",
-            "--efuses", f"{tree_dir}/{SRK_EFUSES}",
-            "--certs", csv_certs, ]
+    cmd = ["/usr/local/bin/srktool",
+           "--table", f"{tree_dir}/{srk_table}",
+           "--efuses", f"{tree_dir}/{SRK_EFUSES}",
+           "--certs", csv_certs, ]
     for word in hab_ver.split():
         cmd.append(word)
     for word in f"--{srk_digest}".split():
@@ -114,6 +115,7 @@ def generate_srks(hab_type, tree_dir):
                                 stderr=subprocess.DEVNULL, check=False)
     if cmd_result.returncode != 0:
         raise RuntimeError("Failed to generate SRK table and hashes")
+
 
 # See `Code-Signing Tool User's Guide` inside CST release package for details
 def new(body, user):
